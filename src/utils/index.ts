225df4cfae19def3f14a23f061740b99c9f3c48d
Yash Chapaneri <yashtatsme@ymail.com>
2025-02-25 22:39:40 +0100

    Removes credentials file and store it as base64 and decode in the app

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,45 +2,56 @@ import { google } from 'googleapis';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function subscribeEmailGoogleSheer(data: any) {
-  const auth = new google.auth.GoogleAuth({
-    keyFile:
-      './roast-atlas-d3de5f078b20.json',
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
+  const env_creds = process.env.GOOGLE_API_CREDS;
+  if (env_creds) {
+    const credentials = Buffer.from(env_creds, 'base64').toString('utf-8');
 
-  const sheets = google.sheets({ version: 'v4', auth });
-  const spreadsheetId = '1kVGIG9pu6Tfmv9JW0MfCAO0_ypYdFUYCJz-ldhaVGr0';
-  const range = 'Sheet1!A1';
+    const auth = new google.auth.GoogleAuth({
+      credentials: JSON.parse(credentials),
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
 
-  const email = data.email;
+    const sheets = google.sheets({ version: 'v4', auth });
+    const spreadsheetId = '1kVGIG9pu6Tfmv9JW0MfCAO0_ypYdFUYCJz-ldhaVGr0';
+    const range = 'Sheet1!A1';
 
-  await sheets.spreadsheets.values.append({
-    spreadsheetId,
-    range,
-    valueInputOption: 'RAW',
-    requestBody: {
-      values: [[email]],
-    },
-  });
+    const email = data.email;
+
+    await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range,
+      valueInputOption: 'RAW',
+      requestBody: {
+        values: [[email]],
+      },
+    });
+  } else {
+    console.log('Credentials not found');
+  }
 }
 
 export async function likeGoogleSheet() {
-  const auth = new google.auth.GoogleAuth({
-    keyFile:
-      './roast-atlas-d3de5f078b20.json',
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
+  const env_creds = process.env.GOOGLE_API_CREDS;
+  if (env_creds) {
+    const credentials = Buffer.from(env_creds, 'base64').toString('utf-8');
+    const auth = new google.auth.GoogleAuth({
+      credentials: JSON.parse(credentials),
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
 
-  const sheets = google.sheets({ version: 'v4', auth });
-  const spreadsheetId = '1kVGIG9pu6Tfmv9JW0MfCAO0_ypYdFUYCJz-ldhaVGr0';
-  const range = 'Sheet2!A:B';
+    const sheets = google.sheets({ version: 'v4', auth });
+    const spreadsheetId = '1kVGIG9pu6Tfmv9JW0MfCAO0_ypYdFUYCJz-ldhaVGr0';
+    const range = 'Sheet2!A:B';
 
-  await sheets.spreadsheets.values.append({
-    spreadsheetId,
-    range,
-    valueInputOption: 'RAW',
-    requestBody: {
-      values: [[1, new Date().toISOString()]],
-    },
-  });
+    await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range,
+      valueInputOption: 'RAW',
+      requestBody: {
+        values: [[1, new Date().toISOString()]],
+      },
+    });
+  } else {
+    console.log('Credentials not found');
+  }
 }
